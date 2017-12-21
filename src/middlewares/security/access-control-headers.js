b01ae4409b914561b04f6780ae3963fde8386880
Mikita Isakau <mikita_isakau@epam.com>
2017-12-21 12:26:58 +0300

    Ready swagger task

--- src/middlewares/security/access-control-headers.js
+++ src/middlewares/security/access-control-headers.js
@@ -1,7 +1,6 @@
 export default function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET? POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With,' +
-    ' Content-Type, Accept, X-Access-Token');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token, X-Access-Token, x-access-token');
   next();
 }
